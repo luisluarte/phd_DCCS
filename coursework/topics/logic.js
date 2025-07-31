7e362c6dc698b6f5148d4cb4b60d54eb7e9dba54
nicolasluarte <64244503+nicolasluarte@users.noreply.github.com>
2025-07-31 18:28:30 -0400

    correctly saves the .csv

--- coursework/topics/logic.js
+++ coursework/topics/logic.js
@@ -1,16 +1,34 @@
-// instruction screens
+// functions
+
+// instruction screen
 function createInstructionScreen(title, content){
 	return new lab.html.Screen({
 		title: title,
 		content: content,
 		responses: {
-			'keypress': ''
+			'keypress': 'participant_accept'
 		},
 	});
 }
 
+// random participant id
+function createRandomParticipantId(){
+
+}
+
 // build the experiment
 const study = new lab.flow.Sequence({
+	// esc to quit the experiment when the participant chooses to
+	events: {
+		'keydown': function(e){
+			if (e.key == 'Escape'){
+				this.options.datastore.download('csv', 'test.csv');
+				this.end();
+			}
+		}
+	},
+
+	// sequence of the experiment
 	content: [
 		createInstructionScreen('Instructions', 'Instructions for a two-armed bandit')
 	]
